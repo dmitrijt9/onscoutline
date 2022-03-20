6c724c22cd02ea267e2c01a3e67aa258bf4b3960
Dmitrij Tkacenko <dimot9@gmail.com>
2022-03-20 14:11:53 +0100

    ✨ Add player in game statistic entity #14

---
+++ services/api/src/entities/PlayerGameStatistic.ts
@@ -0,0 +1,34 @@
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm'
+import { PlayerInMatch } from './Relations/PlayerInMatch'
+
+@Entity()
+export class PlayerGameStatistic {
+    @PrimaryGeneratedColumn('uuid')
+    id: string
+
+    @Column('varchar')
+    statType: StatType
+
+    @ManyToOne(() => PlayerInMatch)
+    playerInMatch: PlayerInMatch
+
+    /**
+     * Value of a stat type.
+     */
+    @Column()
+    value: number
+
+    /**
+     * Minute of a match when the stat happened.
+     * In case of total played minutes in a match there is null value, because it does not have particular minute of stat happening.
+     */
+    @Column('int', { nullable: true })
+    minute: number | null
+}
+
+export enum StatType {
+    Goal = 'Goal',
+    Assist = 'Assist',
+    YellowCard = 'YellowCard',
+    RedCard = 'RedCard',
+}
