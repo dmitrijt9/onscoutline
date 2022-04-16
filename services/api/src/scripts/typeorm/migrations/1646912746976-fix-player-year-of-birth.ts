497cdad2b6fafbac28b39e390dcbdec087ece7bf
Dmitrij Tkacenko <dimot9@gmail.com>
2022-04-16 17:45:38 +0200

    🚨

--- services/api/src/scripts/typeorm/migrations/1646912746976-fix-player-year-of-birth.ts
+++ services/api/src/scripts/typeorm/migrations/1646912746976-fix-player-year-of-birth.ts
@@ -1,18 +1,21 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm'
 
 export class fixPlayerYearOfBirth1646912746976 implements MigrationInterface {
     name = 'fixPlayerYearOfBirth1646912746976'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE \`player\` CHANGE \`dateOfBirth\` \`yearOfBirth\` date NOT NULL`);
-        await queryRunner.query(`ALTER TABLE \`player\` DROP COLUMN \`yearOfBirth\``);
-        await queryRunner.query(`ALTER TABLE \`player\` ADD \`yearOfBirth\` varchar(255) NOT NULL`);
+        await queryRunner.query(
+            `ALTER TABLE \`player\` CHANGE \`dateOfBirth\` \`yearOfBirth\` date NOT NULL`,
+        )
+        await queryRunner.query(`ALTER TABLE \`player\` DROP COLUMN \`yearOfBirth\``)
+        await queryRunner.query(`ALTER TABLE \`player\` ADD \`yearOfBirth\` varchar(255) NOT NULL`)
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE \`player\` DROP COLUMN \`yearOfBirth\``);
-        await queryRunner.query(`ALTER TABLE \`player\` ADD \`yearOfBirth\` date NOT NULL`);
-        await queryRunner.query(`ALTER TABLE \`player\` CHANGE \`yearOfBirth\` \`dateOfBirth\` date NOT NULL`);
+        await queryRunner.query(`ALTER TABLE \`player\` DROP COLUMN \`yearOfBirth\``)
+        await queryRunner.query(`ALTER TABLE \`player\` ADD \`yearOfBirth\` date NOT NULL`)
+        await queryRunner.query(
+            `ALTER TABLE \`player\` CHANGE \`yearOfBirth\` \`dateOfBirth\` date NOT NULL`,
+        )
     }
-
 }
